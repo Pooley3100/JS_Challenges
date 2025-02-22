d349978a8581dcd134ed796ac4db93d4348c11cd
mattpoole <matt.a.poole@outlook.com>
2025-02-22 12:37:47 +0000

    infinite canvas

--- GameOfLife/index.js
+++ GameOfLife/index.js
@@ -1,6 +1,10 @@
 var canvasWidth = 400;
 var canvasHeight = 400;
-var squareSize = 14;
+//Stores offset from 0.0 (i.e. universe takes up 80% of screen)
+const universeScale = 0.8;
+var universeWidth = 0;
+var universeHeight = 0;
+const squareSize = 14;
 var constDecrease = 4;
 var cellsMap = [];
 var nextGenCells = [];
@@ -8,13 +12,15 @@ var rowW = 0;
 var colH = 0;
 var countPause = 0;
 
-function cellsMapSetup(){
-    rowW = Math.floor(canvasWidth / squareSize)
-    colH = Math.floor(canvasHeight / squareSize)
-    for(let i = 0; i<rowW; i+=1){
+
+function cellsMapSetup() {
+    //Take up 80% of the screen 
+    rowW = universeWidth / squareSize
+    colH = universeHeight / squareSize
+    for (let i = 0; i < rowW; i += 1) {
         let r = [];
         let r2 = [];
-        for(let j = 0; j<colH; j+=1){
+        for (let j = 0; j < colH; j += 1) {
             r.push(0);
             r2.push(0);
         }
@@ -23,14 +29,19 @@ function cellsMapSetup(){
     }
 }
 
-function setup() { 
+function setup() {
     angleMode(RADIANS);
-    canvasWidth = windowWidth-constDecrease;
-    canvasHeight = windowHeight-constDecrease;
+    //Const decrease as canvas seen as slightly bigger then actual window
+    canvasWidth = windowWidth - constDecrease;
+    canvasHeight = windowHeight - constDecrease;
+    universeWidth = Math.floor((canvasWidth * universeScale) / squareSize) * squareSize;
+    universeHeight = Math.floor((canvasHeight * universeScale) / squareSize) * squareSize;
     createCanvas(canvasWidth, canvasHeight);
     cellsMapSetup();
     background(100);
     frameRate(15);
+    textSize(35)
+    textAlign(CENTER, CENTER);
 
     //Initial cool seed
     cellsMap[8][4] = 1
@@ -41,114 +52,118 @@ function setup() {
 }
 
 function windowResized() {
+    canvasWidth = windowWidth - constDecrease;
+    canvasHeight = windowHeight - constDecrease;
+    universeWidth = Math.floor((canvasWidth * universeScale) / squareSize) * squareSize;
+    universeHeight = Math.floor((canvasHeight * universeScale) / squareSize) * squareSize;
     cellsMapSetup();
-    canvasWidth = windowWidth -constDecrease;
-    canvasHeight = windowHeight -constDecrease;
     resizeCanvas(canvasWidth, canvasHeight);
 }
 
-function copyFromNextGen(){
-    for(let i = 0; i<rowW; i+=1){
-        for(let j = 0; j<colH; j+=1){
+function copyFromNextGen() {
+    for (let i = 0; i < rowW; i += 1) {
+        for (let j = 0; j < colH; j += 1) {
             cellsMap[i][j] = nextGenCells[i][j];
         }
     }
 }
 
 //The meat of it
-function generation(){
+function generation() {
     //Rule 1, any live cell with fewer than two live neighbours dies
     //Rule 2, any live cell with two or three live neighbours lives on to the next generation
     //Rule 3, any live cell with more than three live neighbours dies as if by overpopulation
     //Rule 4, any dead cell with exactly three live neighbours become a live cell
-    for(let i = 0; i<rowW; i+=1){
-        for(let j = 0; j<colH; j+=1){
-            let neighboursAlive = 0
-            if(cellsMap[i][j] == 1){
-                console.log(`Cell: ${i} ${j} with neighbours alive: ${neighboursAlive}`)
+    for (let i = 0; i < rowW; i += 1) {
+        for (let j = 0; j < colH; j += 1) {
+            let neighboursAlive = 0;
+
+            // Check all eight neighbors
+            const neighbors = [
+                [(i - 1 + rowW) % rowW, (j - 1 + colH) % colH],
+                [(i - 1 + rowW) % rowW, j],
+                [(i - 1 + rowW) % rowW, (j + 1) % colH],
+                [i, (j - 1 + colH) % colH],
+                [i, (j + 1) % colH],
+                [(i + 1) % rowW, (j - 1 + colH) % colH],
+                [(i + 1) % rowW, j],
+                [(i + 1) % rowW, (j + 1) % colH]
+            ];
+
+            for (const [x, y] of neighbors) {
+                if (cellsMap[x][y] == 1) {
+                    neighboursAlive += 1;
+                }
             }
-            if(i>0 && cellsMap[i-1][j] == 1){
-                neighboursAlive+=1
-            }
-            if(i<rowW-1 && cellsMap[i+1][j] == 1){
-                neighboursAlive+=1
-            }
-            if(j>0 && cellsMap[i][j-1] == 1){
-                neighboursAlive+=1
-            }
-            if(j<colH-1 && cellsMap[i][j+1] == 1){
-                neighboursAlive+=1
-            }
-            //Diagonals
-            if(j<colH-1 && i>0 && cellsMap[i-1][j+1] == 1){
-                neighboursAlive+=1
-            }
-            if(j>0 && i>0 && cellsMap[i-1][j-1] == 1){
-                neighboursAlive+=1
-            }
-            if(j<colH-1 && i<rowW-1 && cellsMap[i+1][j+1] == 1){
-                neighboursAlive+=1
-            }
-            if(j>0 && i<rowW-1 && cellsMap[i+1][j-1] == 1){
-                neighboursAlive+=1
-            }
-            
-            //Now the rules
-            if(cellsMap[i][j] == 1){
-                console.log(`Cell: ${i} ${j} with neighbours alive: ${neighboursAlive}`)
-            }
-            nextGenCells[i][j] = cellsMap[i][j]
-            if(cellsMap[i][j] == 1){
-                if(neighboursAlive < 2){
+
+            // Apply the rules of the game
+            if (cellsMap[i][j] == 1) {
+                if (neighboursAlive < 2 || neighboursAlive > 3) {
                     nextGenCells[i][j] = 0;
-                }else if(neighboursAlive > 3){
+                } else {
+                    nextGenCells[i][j] = 1;
+                }
+            } else {
+                if (neighboursAlive == 3) {
+                    nextGenCells[i][j] = 1;
+                } else {
                     nextGenCells[i][j] = 0;
                 }
-            } else if(neighboursAlive == 3){
-                nextGenCells[i][j] = 1;
             }
         }
     }
     copyFromNextGen();
 }
 
-function drawGrid(){
+function drawGrid() {
     //Vertical Lines
-    for(let i = 0; i<rowW; i+=1){
-        line(i*squareSize, 0, i*squareSize, canvasHeight);
+    for (let i = 0; i < rowW + 1; i += 1) {
+        line(i * squareSize, 0, i * squareSize, universeHeight);
     }
     //Horizontal Lines
-    for(let i = 0; i<colH; i+=1){
-        line(0, i*squareSize, canvasWidth, i*squareSize);
+    for (let i = 0; i < colH + 1; i += 1) {
+        line(0, i * squareSize, universeWidth, i * squareSize);
     }
 
 }
 
-function mouseClicked(){
-    let x = Math.floor(mouseX/squareSize)
-    let y = Math.floor(mouseY/squareSize)
-    cellsMap[x][y] = 1
+function mouseClicked() {
+    let x = Math.floor((mouseX - ((canvasWidth - universeWidth) / 2)) / squareSize);
+    let y = Math.floor((mouseY - ((canvasHeight - universeHeight) / 2)) / squareSize);
+    try {
+        cellsMap[x][y] = 1
+    } catch (err) {
+        console.log("not clicked within canvas");
+    }
 }
 
-function drawCells(){
+function drawCells() {
     fill(1);
-    for(let i = 0; i<rowW; i+=1){
-        for(let j = 0; j<colH; j+=1){
-            if(cellsMap[i][j] == 1){
-                square(i*squareSize, j*squareSize, squareSize)
+    for (let i = 0; i < rowW; i += 1) {
+        for (let j = 0; j < colH; j += 1) {
+            if (cellsMap[i][j] == 1) {
+                square(i * squareSize, j * squareSize, squareSize)
             }
         }
     }
 }
 
-function draw(){
+function draw() {
     //cellsMap[8][4] == 1 ? cellsMap[8][4] = 0 : cellsMap[8][4] = 1
-    
+
     background(250);
+    
+    text("Conwy Game of Life", canvasWidth/2, 40);
+
+    //Universe
+    translate(canvasWidth * ((1 - universeScale) / 2), canvasHeight * ((1 - universeScale) / 2));
     drawGrid();
-    if(countPause > 40){
+    if (countPause > 40) {
         generation();
+    } else{
+        text("Select Cells to Be Alive", canvasWidth/2, 10);
     }
-    countPause+=1
+    countPause += 1
     drawCells();
+    translate(0, 0);
 }
